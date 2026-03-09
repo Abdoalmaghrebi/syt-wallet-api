1099a3611c61fc9ab3e5d624b4ac4ec2c9dafce4
Abdullrahman almaghrabi <abdprincelove18@gmail.com>
2026-03-10 00:05:43 +0300

    Fix cors import in index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const cors = require('express');
-
+const cors = require('cors');
 // ✅ استيراد البوت
 const bot = require('./bot');
 
